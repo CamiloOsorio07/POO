040e22f9523628f600a56880cec059c257765c6a
CamiloOsorio07 <112044657+CamiloOsorio07@users.noreply.github.com>
2022-10-19 19:55:10 -0500

    Update EJERCICIO10.java

--- src/EJERCICIOS/EJERCICIO10.java
+++ src/EJERCICIOS/EJERCICIO10.java
@@ -9,13 +9,13 @@ public class EJERCICIO10 {
 	public static void main(String[] args) {
 		
 	    System.out.print("Introduzca la altura de la pirámide: ");
-	    int altusuario = keyboard.nextInt();
+	    int alturadefusuario = keyboard.nextInt();
 
 	    int altura = 1;
 	    int i = 0;
-	    int espacios = altusuario - 1;
+	    int espacios = alturadefusuario - 1;
 	    
-	    while (altura <= altusuario) {
+	    while (altura <= alturadefusuario) {
 	      
 	      for (i = 1; i <= espacios; i++) {
 	        System.out.print(" ");
